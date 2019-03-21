11c37d92259f1068edbaf9753d60d9b04ec29e57
Dixzz <github.com/Dixzz>
2019-03-21 16:41:09 +0000

    Update the pathmap

--- Android.mk
+++ Android.mk
@@ -1,13 +1,10 @@
+ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf-msm8996)
+
 QCOM_MEDIA_ROOT := $(call my-dir)
 
-#Compile these for all targets under QCOM_BOARD_PLATFORMS list.
-ifeq ($(call is-board-platform-in-list, $(QCOM_BOARD_PLATFORMS)),true)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
-include $(QCOM_MEDIA_ROOT)/libaac/Android.mk
-endif
 
-ifeq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)),true)
 include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
 
 ifeq ($(BOARD_USES_ADRENO), true)
@@ -16,8 +13,23 @@ endif
 
 include $(QCOM_MEDIA_ROOT)/hypv-intercept/Android.mk
 
-ifeq ($(TARGET_BOARD_AUTO),true)
-include $(QCOM_MEDIA_ROOT)/libsidebandstreamhandle/Android.mk
 endif
 
-endif
+#QCOM_MEDIA_ROOT := $(call my-dir)
+
+#include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
+#include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
+#include $(QCOM_MEDIA_ROOT)/libaac/Android.mk
+
+#include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
+
+#ifeq ($(BOARD_USES_ADRENO), true)
+#include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
+#endif
+
+#include $(QCOM_MEDIA_ROOT)/hypv-intercept/Android.mk
+
+#ifeq ($(TARGET_BOARD_AUTO),true)
+#include $(QCOM_MEDIA_ROOT)/libsidebandstreamhandle/Android.mk
+#endif
+
